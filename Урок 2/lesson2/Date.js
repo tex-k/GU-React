a143223251da149b7ae725ac663de61de3c9c7bb
Aleksey Pivovarov <texpiv@gmail.com>
2017-12-12 15:17:52 +0300

    Quotes are corrected. File .gitgnore is added.

--- Урок 2/lesson2/Date.js
+++ Урок 2/lesson2/Date.js
@@ -1,20 +1,20 @@
 module.exports = {
     write: () => {
         const div = document.createElement('div');
-        div.style.width = "100px";
-        div.style.height = "50px";
-        div.style.backgroundColor = "green";
-        div.style.borderRadius = "10px";
+        div.style.width = '100px';
+        div.style.height = '50px';
+        div.style.backgroundColor = 'green';
+        div.style.borderRadius = '10px';
         div.style.boxSizing = 'border-box';
-        div.style.paddingTop = "7px";
-        div.style.textAlign = "center";
+        div.style.paddingTop = '7px';
+        div.style.textAlign = 'center';
         div.style.color = 'blue';
-        div.style.fontSize = "30px";
+        div.style.fontSize = '30px';
         div.innerText = 'Дата';
-        div.addEventListener("mouseover", function () {
-            div.style.cursor = "pointer";
+        div.addEventListener('mouseover', function () {
+            div.style.cursor = 'pointer';
         });
-        div.addEventListener("click", function () {
+        div.addEventListener('click', function () {
             alert(new Date());
         });
         document.body.appendChild(div);
